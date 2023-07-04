7488a34e686da27d0ae5be417ae33697af279b46
thiyahoo <thiyagu.multi@gmail.com>
2023-07-04 22:08:59 +0400

    Ingore File Added

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log(
-      "Database connected: ",
-      connect.connection.host,
-      connect.connection.name
-    );
+    console.log("Database connected");
+    
   } catch (err) {
     console.log(err);
     process.exit(1);
